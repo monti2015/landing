e0d38c0504d0fb35b901e874c01388a72bc8cf91
“me” <me@local>
2020-05-24 11:42:45 +0000

    working font

--- main/settings.py
+++ main/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'th' #'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -113,6 +113,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+   os.path.join(BASE_DIR, 'locale')
+]
 
 
 # Static files (CSS, JavaScript, Images)
